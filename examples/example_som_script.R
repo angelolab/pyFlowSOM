05278b1b75f84350d7f59e8810099964c699cac3
y2k <git@y2kbugger.com>
2022-10-27 20:47:22 -0400

    Initial commit of example files

---
+++ examples/example_som_script.R
@@ -0,0 +1,49 @@
+# Example script to run FlowSOM in R
+
+# Download FlowSOM from github (if need be, install devtools with install.packages("devtools"))
+devtools::install_github("SofieVG/FlowSOM", build_vignettes=FALSE)
+
+# Load FlowSOM package
+library(FlowSOM)
+
+# FlowSOM is stocahstic, set seed here
+set.seed(2022)
+
+# Read in table (rows are observations (either pixels of cells), columns are channels (proteins))
+dat = read.csv("example_som_input.csv")
+
+# Cluster using FlowSOM
+som_out = SOM(data=as.matrix(dat), rlen=10, xdim=10, ydim=10, map=FALSE)
+
+# Can use this function to inspect the output of SOM
+str(som_out)
+
+# Extract SOM nodes (what we want from the output), example output nodes in example_node_output.csv
+# SOM is stochastic so your output may be different
+nodes = som_out$codes
+write.csv(nodes, "example_node_output_script.csv", row.names=FALSE)
+
+# Can use those nodes to map unseen data (here, just demonstrating using the original data)
+# This function is not stochastic
+read_in_nodes = read.csv("example_node_output.csv")
+mapping_out = FlowSOM:::MapDataToCodes(as.matrix(read_in_nodes), as.matrix(dat))
+
+# From this output, we can extract the cluster for each observation
+# Using example_node_output.csv, you should be able to get the same clusters
+clusters = mapping_out[,1]
+dat$cluster = clusters
+write.csv(dat, "example_clusters_output_script.csv", row.names=FALSE)
+
+
+
+## Make heatmap of outputs to assess clustering
+clust_mean = aggregate(. ~ cluster, dat, mean)
+clust_mean$cluster = NULL
+clust_mean = scale(clust_mean)
+
+# If need be, install pheatmap using install.packages("pheatmap")
+library(pheatmap)
+pdf("example_heatmap_script.pdf")
+pheatmap(clust_mean, breaks=seq(-3,3,length.out=99))
+dev.off()
+
