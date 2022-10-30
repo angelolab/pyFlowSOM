2f47e975463439156da45bc72e9b30fc3e4523f0
y2k <git@y2kbugger.com>
2022-10-30 12:15:20 -0400

    Fix deprecated packaging

--- setup.py
+++ setup.py
@@ -5,6 +5,4 @@ pyflowsom_module = Extension('pyflowsom',
                     sources = ['pyflowsom.c'])
 
 setup (name = 'pyflowsom',
-       version = '1.0',
-       description = 'Python wrapper for the FlowSOM algorithm.',
        ext_modules = [pyflowsom_module])
