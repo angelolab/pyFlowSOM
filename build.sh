fd18c73063de276c851fb1a196bad4426c3a40d8
y2k <git@y2kbugger.com>
2022-10-31 23:03:33 -0400

    Use --editable style install for build.sh && test.sh

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ echo "### build.sh starting ###"
 if [ ! -d "./.pyflowsom-venv" ]; then
     python3 -m venv .pyflowsom-venv
     pip install --upgrade pip setuptools wheel
+    pip install --editable .[tests]
 fi
 
 source ./.pyflowsom-venv/bin/activate
 
-pip install .[tests]
+python setup.py develop
 
 echo "### build.sh finished ###"
