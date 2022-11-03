8c9886f9f582f01dbee4bdfd5a0683ec17feb417
y2k <git@y2kbugger.com>
2022-11-02 21:47:02 -0400

    Don't install pyFlowSOM at same time as requirements
    If build fails then nothing gets installed but reinstall isn't triggered by build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo "### build.sh starting ###"
 if [ ! -d "./.pyflowsom-venv" ]; then
     python3 -m venv .pyflowsom-venv
     pip install --upgrade pip setuptools wheel
-    pip install --editable .[tests]
+    pip install -r requirements.txt -r requirements-test.txt
 fi
 
 source ./.pyflowsom-venv/bin/activate
