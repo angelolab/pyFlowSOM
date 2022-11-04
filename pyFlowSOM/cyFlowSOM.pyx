5fb8b6eac2422a6fd4ed51f82b2714e33934bc49
y2k <git@y2kbugger.com>
2022-11-04 19:52:05 -0400

    Stub of Cython binding

--- pyFlowSOM/cyFlowSOM.pyx
+++ pyFlowSOM/cyFlowSOM.pyx
@@ -1,6 +1,28 @@
 cdef extern from "flowsom.c":
-    cpdef int c_plus_seven(int a)
-    void c_square_each(double *, unsigned int)
+    void C_square_each(double *, unsigned int)
+    void C_SOM(
+        double *data,
+        double *codes,
+        double *nhbrdist,
+        double *alphas,
+        double *radii,
+        double *xdists,
+        int *pn,
+        int *ppx,
+        int *pncodes,
+        int *prlen,
+        int *dist
+        )
+    void C_mapDataToCodes(
+        double *data,
+        double *codes,
+        int *pncodes,
+        int *pnd,
+        int *pp,
+        int *nnCodes,
+        double *nnDists,
+        int *dist
+        )
 
 import numpy as np
 
@@ -13,7 +35,27 @@ def square_each(np_mat):
     if not mat.shape[0] == mat.shape[1]:
         raise ValueError("Matrix must be square")
 
-    c_square_each(&mat[0, 0], mat.shape[0])
+    C_square_each(&mat[0, 0], mat.shape[0])
 
     return mat
 
+def som(
+    data,
+    xdim = 10,
+    ydim = 10,
+    rlen = 10,
+    mst = 1,
+    alpha = [0.05, 0.01],
+    radius = lambda dist: [np.percentile(dist, 0.67), 0],
+    init = False,
+    initf = None,
+    distf = 2,
+    silent = False,
+    map = False,
+    codes = None,
+    importance = None,
+    ):
+    pass
+
+def map_data_to_codes(data, codes, distf = 2):
+    pass
